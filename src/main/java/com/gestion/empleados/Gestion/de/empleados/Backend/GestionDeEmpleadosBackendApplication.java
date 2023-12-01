4d99a900e5c1ada064c59dc4db8a34374de5001a
marilynpb <g.marilyn182@hotmail.com>
2023-12-01 18:25:23 -0300

    First commit

---
+++ src/main/java/com/gestion/empleados/Gestion/de/empleados/Backend/GestionDeEmpleadosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gestion.empleados.Gestion.de.empleados.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeEmpleadosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeEmpleadosBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestion/empleados/Gestion/de/empleados/Backend/GestionDeEmpleadosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestion.empleados.Gestion.de.empleados.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeEmpleadosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
